7180bf25b011ea3b55504b9cb750cfed569f0c62
nahid <nahidkishore99@gmail.com>
2020-10-13 00:07:56 +0600

    Store image directly on mongodb and display them

--- src/components/Home/Doctors/DoctorCard.js
+++ src/components/Home/Doctors/DoctorCard.js
@@ -6,19 +6,16 @@ const DoctorCard = ({ doctor }) => {
   /*  const{name,phone,img}=props.doctor; */
   return (
     <div className="col-md-4 col-sm-6 text-center">
-      {doctor.image ? (
-        <img
-          style={{ height: "200px" }}
-          src={`data:image/png;base64,${doctor.image.img}`}
-        />
-      ) : (
+      {doctor.image ? 
+  <img style={{ height: "200px" }} src={`data:image/png;base64,${doctor.image.img}`} alt="" />
+       : 
         <img
           style={{ height: "200px" }}
           className="img-fluid mb-3"
           src={`http://localhost:5000/${doctor.img}`}
           alt=""
         />
-      )}
+      }
       <h4>{doctor.name}</h4>
       <p>
         {" "}
